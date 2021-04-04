71607bc2fa0791e920019b0c7040534f3606f7f1
danbudworthmead <danbudworthmead@gmail.com>
2021-04-04 13:27:30 +0100

    Revert "Fix a bug causing InnerPlayerInfo.Controller to be always null"
    This reverts commit 40a41f357338b190dbeb8c629d6910380fc23691.

--- src/Impostor.Server/Net/State/Game.Data.cs
+++ src/Impostor.Server/Net/State/Game.Data.cs
@@ -329,11 +329,16 @@ namespace Impostor.Server.Net.State
                     player.DisableSpawnTimeout();
 
                     // Hook up InnerPlayerControl <-> InnerPlayerControl.PlayerInfo.
-                    var playerInfo = GameNet.GameData!.GetPlayerById(control.PlayerId) ?? GameNet.GameData.AddPlayer(control);
+                    var playerInfo = GameNet.GameData!.GetPlayerById(control.PlayerId);
 
                     if (playerInfo != null)
                     {
-                        control.PlayerInfo.Controller = control;
+                        playerInfo.Controller = control;
+                        control.PlayerInfo = playerInfo;
+                    }
+                    else
+                    {
+                        GameNet.GameData.AddPlayer(control);
                     }
 
                     await _eventManager.CallAsync(new PlayerSpawnedEvent(this, player, control));
