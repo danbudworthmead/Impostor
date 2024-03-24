033b6ed7847f0450bfc40dc0b1a30e68fa42d260
danbudworthmead <danbudworthmead@gmail.com>
2024-03-24 10:32:32 +0000

    Revert "wip"
    This reverts commit 81bb86c46646c1fc20339924dcd230bb10068ad9.

---
+++ src/Impostor.Server/Events/Game/Player/PlayerSabotagedEvent.cs
@@ -0,0 +1,29 @@
+﻿using Impostor.Api.Events.Player;
+using Impostor.Api.Games;
+using Impostor.Api.Innersloth;
+using Impostor.Api.Net;
+using Impostor.Api.Net.Inner.Objects;
+
+namespace Impostor.Server.Events.Player;
+
+public class PlayerSabotagedEvent : IPlayerSabotagedEvent
+{
+    public PlayerSabotagedEvent(IGame game, IClientPlayer clientPlayer, IInnerPlayerControl playerControl, bool isCancelled, SystemTypes type)
+    {
+        Game = game;
+        ClientPlayer = clientPlayer;
+        PlayerControl = playerControl;
+        IsCancelled = isCancelled;
+        Type = type;
+    }
+
+    public IGame Game { get; }
+
+    public IClientPlayer ClientPlayer { get; }
+
+    public IInnerPlayerControl PlayerControl { get; }
+
+    public bool IsCancelled { get; set; }
+
+    public SystemTypes Type { get; }
+}
