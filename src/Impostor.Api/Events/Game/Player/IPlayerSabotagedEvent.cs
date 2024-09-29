8eafe8c70b6821257fe403ac5513d30d70605cfc
danbudworthmead <danbudworthmead@gmail.com>
2024-09-29 14:52:30 +0100

    Create IPlayerSabotagedEvent.cs

---
+++ src/Impostor.Api/Events/Game/Player/IPlayerSabotagedEvent.cs
@@ -0,0 +1,10 @@
+﻿using Impostor.Api.Innersloth;
+
+namespace Impostor.Api.Events.Player;
+
+public interface IPlayerSabotagedEvent : IPlayerEvent
+{
+    SystemTypes Type { get; }
+
+    bool IsCancelled { get; set; }
+}
