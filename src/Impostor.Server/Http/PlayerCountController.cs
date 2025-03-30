321630b76d078fed2c742b3129946f692dd3635b
danbudworthmead <danbudworthmead@gmail.com>
2025-03-30 22:29:14 +0100

    expose the playercount via an API

---
+++ src/Impostor.Server/Http/PlayerCountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Impostor.Api.Net.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Impostor.Server.Http;
+
+/// <summary>
+/// This controller has a method to get the player count.
+/// </summary>
+[Route("/api/playercount")]
+[ApiController]
+public sealed class PlayerCountController : ControllerBase
+{
+    IClientManager _iClientManager;
+
+    public PlayerCountController(IClientManager iClientManager)
+    {
+        _iClientManager = iClientManager;
+    }
+
+    /// <summary>
+    /// Get the player count.
+    /// </summary>
+    /// <returns>The player count.</returns>
+    [HttpGet]
+    public IActionResult GetPlayerCount()
+    {
+        // get all the connected clients
+        return Ok(_iClientManager.Clients.Count());
+    }
+}
